b66aa17e5751c1c4cb8d698912ca27ffd5859745
Balajisaketh <70933350+Balajisaketh@users.noreply.github.com>
2023-10-11 22:12:12 +0530

    done

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -45,6 +45,30 @@ footer{
   height: auto;
   margin-top: 20px;
 }
+.fontdata{
+  font-size: 24px;
+}
+.follower{
+  font-size: 14px;
+}
+.para{
+  font-size: 14px;
+  line-height: 20px;
+}
+.parahead{
+  font-family: 'Poppins', sans-serif; 
+  font-weight: 500;
+  font-size: 24px;
+  color: #333333;
+  line-height: 26px;
+}
+.news{
+  font-style: normal;
+  font-weight: 700;
+  line-height: 20px;
+  font-size: 14px;
+  font-family: 'Poppins', sans-serif; 
+}
 </style>
 
 <!-- for mobile devices -->
@@ -89,14 +113,14 @@ footer{
         <h2 class="family">name</h2>
         <div class="d-flex flex-row">
 
-          <p class="family">Following<span>:0</span></p>
-          <p class="mx-2 family">Followers<span>:0</span></p>
+          <p class="family follower">Following<span>:0</span></p>
+          <p class="mx-2 family follower">Followers<span>:0</span></p>
         </div>
 
-   <p class="family">Equity Rank:<span class="mx-2">N/A</span></p>
-              <p class="d-md-none d-lg-block family">Lifetime Games :<span class="mx-1">0</span></p>
+   <p class="family follower">Equity Rank:<span class="mx-2">N/A</span></p>
+              <p class="d-md-none d-lg-block family follower">Lifetime Games :<span class="mx-1">0</span></p>
               <!-- <p class="d-md-block d-lg-none w-[4%] family">Games in Lifetime:<span>0</span></p> -->
-              <p class="family">Portfolio:<span class="mx-2">$300000</span></p>
+              <p class="family follower">Portfolio:<span class="mx-2">$300000</span></p>
       </div>
     </div>
     <div >
@@ -105,15 +129,15 @@ footer{
       <div class="d-flex flex-row justify-content-center  gx-2 col-12 mt-1 ">
         <div class="d-flex flex-row col-3 mx-3">
         <input type="radio"/>
-        <p class="mt-3 mx-2">Equities/Etf</p>
+        <p class="mt-3 mx-2 follower">Equities/Etf</p>
         </div>
         <div class="d-flex flex-row col-3 mx-3" >
           <input type="radio"/>
-          <p class="mt-3 mx-2">Forex  </p>
+          <p class="mt-3 mx-2 follower">Forex  </p>
           </div>
           <div class="d-flex flex-row col-4 mx-3">
             <input type="radio"/>
-            <p class="mt-3 mx-2">Cryptocurrencis</p>
+            <p class="mt-3 mx-2 follower">Cryptocurrencis</p>
             </div>
             
            
@@ -122,8 +146,8 @@ footer{
       <hr>
       <div class="d-flex flex-row col-12 border">
         <div class="d-flex flex-row justify-content-center  ">
-          <button class=" btnbg m-2 py-2 px-2 text-white rounded border-0">Play DayTrader Simulation</button>
-          <button class="btnbg m-2 py-2 px-2 text-white rounded border-0">Play SwingTrader Simulation</button>
+          <button class=" btnbg m-2 py-2 px-2 text-white rounded border-0 follower family">Play DayTrader Simulation</button>
+          <button class="btnbg m-2 py-2 px-2 text-white rounded border-0 follower family  ">Play SwingTrader Simulation</button>
       </div>
       </div>
     </div>
@@ -131,7 +155,7 @@ footer{
       <div class="col-3">
          <div class="d-flex flex-row">
           <input type="checkbox" class="m-2"/>
-          <p class="mt-2">practice</p>
+          <p class="mt-2 follower">practice</p>
 
          </div>
       </div>
@@ -142,54 +166,54 @@ footer{
       <hr>
     </div>
 <div class="d-flex flex-row col-12 justify-content-center">
-   <p class="col-2 family">Rank</p>
-   <p class="col-2 family">User</p>
-   <p class="col-3 family">Net Worth</p>
-   <p class="col-2 family">ACR</p>
+   <p class="col-2 family follower">Rank</p>
+   <p class="col-2 family follower">User</p>
+   <p class="col-3 family follower">Net Worth</p>
+   <p class="col-2 family follower">ACR</p>
    
 </div>
 <div class="d-flex flex-row col-12 justify-content-center">
-  <p class="col-2 family">1</p>
-  <p class="col-2 family">User</p>
-  <p class="col-3 family">$356.76 tredecilion</p>
-  <p class="col-2 family">$356.76 </p>
+  <p class="col-2 family follower">1</p>
+  <p class="col-2 family follower">User</p>
+  <p class="col-3 family follower">$356.76 tredecilion</p>
+  <p class="col-2 family follower">$356.76 </p>
   
 </div>
    </div>
    <div class="col-12 mx-2  mt-4 mx-auto border   rounded customborder">
     <div class="heading bg-grey">
-      <p class="py-2 text-sm px-2 fw-bold">News</p>
+      <p class="py-2 text-sm px-2 fw-bold news">News</p>
       <hr>
     </div>
     <div class="d-flex flex-row justify-content-end">
-         <p class="mx-4 my-1">01/12/2019</p>
+         <p class="mx-4 my-1 follower">01/12/2019</p>
          
     </div>
     <div class="mt-2">
       <h3 class="mx-3 fw-normal family">Emergency Maintainane and update</h3>
-      <p class="mx-3 mt-4 fontstyling text-sm">ChartGame will be down on Sunday, January 13th between 7:30pm and 9:30pm PST for an update. All active games will be deleted for this update so please finish your game before this update.
+      <p class="mx-3 mt-4 fontstyling text-sm para">ChartGame will be down on Sunday, January 13th between 7:30pm and 9:30pm PST for an update. All active games will be deleted for this update so please finish your game before this update.
       </p>
 
   </div>
   <hr>
   <div class="d-flex flex-row justify-content-end">
-    <p class="mx-4 my-1">01/12/2019</p>
+    <p class="mx-4 my-1 follower">01/12/2019</p>
     
 </div>
   <div class="mt-2">
     <h3 class="mx-3 fw-normal family">Emergency Maintainane and update</h3>
-    <p class="mx-3 mt-4 fontstyling">ChartGame will be down on Sunday, January 13th between 7:30pm and 9:30pm PST for an update. All active games will be deleted for this update so please finish your game before this update.
+    <p class="mx-3 mt-4 fontstyling para">ChartGame will be down on Sunday, January 13th between 7:30pm and 9:30pm PST for an update. All active games will be deleted for this update so please finish your game before this update.
     </p>
 
 </div>
 <hr>
 <div class="d-flex flex-row justify-content-end">
-  <p class="mx-4 my-1">01/12/2019</p>
+  <p class="mx-4 my-1 follower">01/12/2019</p>
   
 </div>
 <div class="mt-2">
-  <h3 class="mx-3 fw-normal family">Emergency Maintainane and update</h3>
-  <p class="mx-3 mt-4 fontstyling">ChartGame will be down on Sunday, January 13th between 7:30pm and 9:30pm PST for an update. All active games will be deleted for this update so please finish your game before this update.
+  <h3 class="mx-3 fw-normal parahead">Emergency Maintainane and update</h3>
+  <p class="mx-3 mt-4 fontstyling para">ChartGame will be down on Sunday, January 13th between 7:30pm and 9:30pm PST for an update. All active games will be deleted for this update so please finish your game before this update.
   </p>
 
 </div>
@@ -235,28 +259,28 @@ footer{
 
            </div>
            <div class="col-6">
-              <h2 class="family">Name</h2>
+              <h2 class="family fontdata  ">Name</h2>
               <div class="d-flex flex-row d-md-none d-lg-flex">
-              <p class="family">Following<span>:0</span></p>
-              <p class="mx-2 family">Followers<span>:0</span></p>
+              <p class="family follower">Following<span>:0</span></p>
+              <p class="mx-2 family follower">Followers<span>:0</span></p>
               </div>
 
               <div class="column d-md-block d-lg-none ">
-                <p class="family">Following<span>:0</span></p>
-                <p class="family">Followers<span>:0</span></p>
+                <p class="family follower">Following<span>:0</span></p>
+                <p class="family follower">Followers<span>:0</span></p>
               </div>
-              <p class="family">Equity Rank:<span class="mx-2">N/A</span></p>
-              <p class="d-md-none d-lg-block family">Lifetime games :<span class="mx-1">0</span></p>
+              <p class="family follower">Equity Rank:<span class="mx-2">N/A</span></p>
+              <p class="d-md-none d-lg-block family follower">Lifetime games :<span class="mx-1">0</span></p>
               <div class="d-flex flex-row">
                 <div class="column">
-                 <p class="family d-md-block d-lg-none">Lifetimegames:</p>
+                 <p class="family d-md-block d-lg-none follower">Lifetimegames:</p>
 
                 </div>
                 <p  class="px-1 d-lg-none d-md-block">0</p>
                 
               </div>
               
-              <p>Portfolio:<span class="mx-1 family">$300000</span></p>
+              <p class="follower family">Portfolio:<span class="mx-1 family follower  ">$300000</span></p>
             
            </div>
               </div>
@@ -283,23 +307,23 @@ footer{
                   <div class="d-flex flex-row justify-content-center">
                    <div class="d-flex flex-row mx-3">
                     <input type="radio"/>
-                    <p class="pt-2 px-1  mt-2">Equities/Etf</p>
+                    <p class="pt-2 px-1  mt-2 family follower">Equities/Etf</p>
                    </div>
                    <div class="d-flex flex-row mx-3">
                     <input type="radio"/>
-                    <p class="pt-2 px-1  mt-2">Forex</p>
+                    <p class="pt-2 px-1  mt-2 family follower">Forex</p>
                    </div>
                    <div class="d-flex flex-row mx-3">
                     <input type="radio"/>
-                    <p class="pt-2 px-1 mt- mt-2">Cryptocurencies</p>
+                    <p class="pt-2 px-1 mt- mt-2 family follower">Cryptocurencies</p>
                    </div>
                   </div>
                   
                 </div>
                 <hr >
                 <div class="d-flex flex-row justify-content-center">
-                    <button class=" btnbg  m-2 py-2 text-white rounded border-0 ">Play DayTrader Simulation</button>
-                    <button class="btnbg m-2  py-2 text-white rounded border-0 ">Play SwingTrader Simulation</button>
+                    <button class=" btnbg  m-2 py-2 text-white rounded border-0 follower family">Play DayTrader Simulation</button>
+                    <button class="btnbg m-2  py-2 text-white rounded border-0 follower family">Play SwingTrader Simulation</button>
                 </div>
                 <hr>
                 <div class="d-flex flex-row px-5 mx-5">
@@ -327,30 +351,30 @@ footer{
              <div class="row ">
              <div class="col-12 d-flex flex-row mx-auto ">
               <div class="col-2 ">
-                <p class="text-center text-md-center  family">Rank</p>
+                <p class="text-center text-md-center  family follower">Rank</p>
               </div>
                <div class="col-3">
-                   <p class="text-left text-md-center family">User</p>
+                   <p class="text-left text-md-center family follower">User</p>
                </div>
                <div class="col-4">
-                   <p class="text-left text-md-center family">Net Worth</p>
+                   <p class="text-left text-md-center family follower">Net Worth</p>
                </div>
                <div class="col-3">
-                  <p class="text-center text-md-center family">ACR</p>
+                  <p class="text-center text-md-center family follower">ACR</p>
                </div>
              </div>
-             <div class="col-12 d-flex flex-row mx-auto ">
+             <div class="col-12 d-flex flex-row mx-auto">
               <div class="col-2 ">
-                <p class="text-center family">1</p>
+                <p class="text-center family follower">1</p>
               </div>
                <div class="col-3">
-                   <p class="text-left text-md-center family">alexpixe31</p>
+                   <p class="text-left text-md-center family follower">alexpixe31</p>
                </div>
                <div class="col-4">
-                   <p class="text-md-center family">$356.76 tredecilion</p>
+                   <p class="text-md-center family follower">$356.76 tredecilion</p>
                </div>
                <div class="col-3">
-                  <p class="text-center family">364.5%</p>
+                  <p class="text-center family follower">364.5%</p>
                </div>
              </div>
              
@@ -358,12 +382,16 @@ footer{
           </div>
           <div class="col-6 mx-2 mx-auto border   rounded customborder">
             <div class="heading bg-grey">
-            <p class="py-2 text-sm px-2 fw-bold">News</p>
+            <p class="py-2 text-sm px-2 fw-bold  news">News</p>
             <hr>
           </div>
+          <div class="d-flex flex-row justify-content-end">
+            <p class="mx-4 my-1 follower">01/12/2019</p>
+            
+        </div>
             <div class="mt-2">
-              <h3 class="mx-3 fw-normal family">Emergency Maintainane and update</h3>
-              <p class="mx-3 mt-4 fontstyling">ChartGame will be down on Sunday, January 13th between 7:30pm and 9:30pm PST for an update. All active games will be deleted for this update so please finish your game before this update.
+              <h3 class="mx-3 fw-normal parahead">Emergency Maintainane and update</h3>
+              <p class="mx-3 mt-4 fontstyling para">ChartGame will be down on Sunday, January 13th between 7:30pm and 9:30pm PST for an update. All active games will be deleted for this update so please finish your game before this update.
               </p>
 
           </div>
@@ -373,8 +401,8 @@ footer{
             
        </div>
           <div class="mt-2">
-            <h3 class="mx-3 fw-normal family">Emergency Maintainane and update</h3>
-            <p class="mx-3 mt-4 fontstyling">ChartGame will be down on Sunday, January 13th between 7:30pm and 9:30pm PST for an update. All active games will be deleted for this update so please finish your game before this update.
+            <h3 class="mx-3 fw-normal parahead">Emergency Maintainane and update</h3>
+            <p class="mx-3 mt-4 fontstyling para">ChartGame will be down on Sunday, January 13th between 7:30pm and 9:30pm PST for an update. All active games will be deleted for this update so please finish your game before this update.
             </p>
 
         </div>
@@ -384,8 +412,8 @@ footer{
           
      </div>
         <div class="mt-2">
-          <h3 class="mx-3 fw-normal family">Emergency Maintainane and update</h3>
-          <p class="mx-3 mt-4 fontstyling">ChartGame will be down on Sunday, January 13th between 7:30pm and 9:30pm PST for an update. All active games will be deleted for this update so please finish your game before this update.
+          <h3 class="mx-3 fw-normal parahead">Emergency Maintainane and update</h3>
+          <p class="mx-3 mt-4 fontstyling para">ChartGame will be down on Sunday, January 13th between 7:30pm and 9:30pm PST for an update. All active games will be deleted for this update so please finish your game before this update.
           </p>
 
       </div>
