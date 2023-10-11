aff4f94248a0c145c8ec91c1fb9b45e10ccfe918
Balajisaketh <70933350+Balajisaketh@users.noreply.github.com>
2023-10-11 20:29:34 +0530

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,46 @@
+<script>
+	
+	import './styles.css';
+	
+</script>
+
+<div class="app">
+	
+<header>
+
+</header>
+	<main>
+		<slot/>
+	</main>
+
+
+</div>
+
+<style>
+header{
+	width:100vw;
+	background-color: black;
+}
+	main {
+		
+
+		height: 100vh;
+		overflow-x: hidden;
+		width: 100vw;
+		padding-top: 0rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
